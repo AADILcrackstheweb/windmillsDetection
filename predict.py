0523e07f08c9fcfb981180da15bed550ab5b38fc
kavinDEV15 <105427639+kavinDEV15@users.noreply.github.com>
2024-07-02 10:20:30 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 import cv2
-model_path = r"C:\Users\sraad\runs\segment\train5\weights\best.pt"
-image_path = r"D:\projct\temp\images\clip15.tif"
+model_path = '/path/to/your/project/directory/'
+image_path = '/path/to/your/project/directory/'
 img = cv2.imread(image_path)
 H, W, _ = img.shape
 model = YOLO(model_path)
@@ -11,5 +11,5 @@ for result in results:
     for j, mask in enumerate(result.masks.data):
         mask = mask.numpy() * 255
         mask = cv2.resize(mask, (W, H))
-        cv2.imwrite('C:/Users/sraad/Pictures/output.png', mask)
+        cv2.imwrite('/path/to/your/project/directory/', mask)
 
