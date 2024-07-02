ded562e26a29387c789a220c1413683a9e5285fc
kavinDEV15 <105427639+kavinDEV15@users.noreply.github.com>
2024-07-02 10:26:12 +0530

    Update trainModel.py

--- trainModel.py
+++ trainModel.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
 
-model.train(data='D:\image-segmentation-yolov8-main\image-segmentation-yolov8-main\config.yaml', epochs=50, imgsz=224)
+model.train(data='/path/to/your/config/file', epochs=50, imgsz=224)
